bebcf9d7d07bad05b6cb4a61b9184d1ff08a89d7
ChaurasiaP <pranshu1431@gmail.com>
2023-02-19 23:30:31 +0530

    ADDED UC1

--- UC.java
+++ UC.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class UC {
+
+    String first;
+    static String VALID_NAME_PATTERN = "^([A-Z]{1}[a-z]{2,})?$";
+
     public static void main(String[] args) {
-        System.out.println("WELCOME TO DAY 19 RegEx Use Cases");
+        Scanner sc = new Scanner(System.in);
+        UC name = new UC();
+
+        System.out.println("Enter first name: ");
+        name.first = sc.nextLine();
+
+        if (name.first.matches(VALID_NAME_PATTERN)) {
+            System.out.println("First Name: " + name.first);
+        } else {
+            System.out.println("Enter Valid Format");
+        }
     }
 }
