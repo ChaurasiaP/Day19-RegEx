5dbae184f3b4a4787302d5916d99b8997076a49e
ChaurasiaP <pranshu1431@gmail.com>
2023-02-19 23:47:23 +0530

    ADDED UC3

--- UC.java
+++ UC.java
@@ -2,24 +2,17 @@ import java.util.Scanner;
 
 public class UC {
 
-    String first, last;
-    static String VALID_NAME_PATTERN = "^([A-Z]{1}[a-z]{2,})?$";
+    static String VALID_EMAIL = "^([a-c]{3}([.|+|-]?[a-zA-Z0-9]+)?@bl.co([.]?[a-zA-Z]{2})?)$";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        UC name = new UC();
+        System.out.println("Enter Email: ");
+        String email = sc.nextLine();
 
-        System.out.println("Enter first name: ");
-        name.first = sc.nextLine();
-
-        System.out.println("Enter last name: ");
-        name.last = sc.nextLine();
-
-        if (name.first.matches(VALID_NAME_PATTERN) && name.last.matches(VALID_NAME_PATTERN)) {
-            System.out.println("First Name: " + name.first);
-            System.out.println("Last Name: " + name.last);
+        if (email.matches(VALID_EMAIL)) {
+            System.out.println(email);
         } else {
-            System.out.println("Enter Valid name");
+            System.out.println("enter again");
         }
     }
 }
