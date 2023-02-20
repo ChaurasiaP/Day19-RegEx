12fef9b09a069a84daac2c289003f2bd8ed1d5ce
Pranshu Chaurasia <66831697+ChaurasiaP@users.noreply.github.com>
2023-02-20 17:55:55 +0530

    ADDED  UC5

--- UC.java
+++ UC.java
@@ -7,7 +7,7 @@ public class UC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter Mobile number: ");
+        System.out.println("Enter Password: ");
         String password = sc.nextLine();
         if (password.matches(VALID_PASSWORD)) {
             System.out.println(password);
